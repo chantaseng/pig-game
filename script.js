88e12bd45c5b2e9dec72e380d9e4eb0513b7997b
William Chan <chan.william.ts@gmail.com>
2024-01-08 19:34:04 -0500

    switching player when we roll 1

--- script.js
+++ script.js
@@ -16,7 +16,9 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 //  Rolling dice functionality
 btnRoll.addEventListener('click', function () {
@@ -32,8 +34,12 @@ btnRoll.addEventListener('click', function () {
   if (dice !== 1) {
     // Add dice to the current score
     currentScore += dice;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+
     current0El.textContent = currentScore; // CHANGE LATER
   } else {
     // Switch to the next player
+    activePlayer = activePlayer === 0 ? 1 : 0;
   }
 });
