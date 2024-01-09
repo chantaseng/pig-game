8e707488d2e2295d45e98b34906c373fd5dc23fe
William Chan <chan.william.ts@gmail.com>
2024-01-08 19:06:45 -0500

    selecting score element DOM

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 'use strict';
+
+// Selecting elements (score on DOM)
+const score0 = document.querySelector('#score--0');
+const score1 = document.querySelector('#score--1');
+
+score0.textContent = 0;
+score1.textContent = 0;
