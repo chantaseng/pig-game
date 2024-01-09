0eba55100b14a0d24c3d0c36ef1e9a5602aaadc8
William Chan <chan.william.ts@gmail.com>
2024-01-08 19:12:08 -0500

    starting condition of pig game, scores at 0 and dice with display value of hidden

--- script.js
+++ script.js
@@ -3,6 +3,9 @@
 // Selecting elements (score on DOM)
 const score0 = document.querySelector('#score--0');
 const score1 = document.querySelector('#score--1');
+const dice = document.querySelector('.dice');
 
+// Starting condition
 score0.textContent = 0;
 score1.textContent = 0;
+dice.classList.add('hidden');
